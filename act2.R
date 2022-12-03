72aee4ae461238961b437d18acfe65cf391caad6
justarandomnameduh <97799964+justarandomnameduh@users.noreply.github.com>
2022-12-03 14:58:31 +0700

    Update act2.R

--- act2.R
+++ act2.R
@@ -112,9 +112,10 @@ legend("topleft", legend = rownames(t(tmpDf)), pch = 15,
        col = 1:nrow(t(tmpDf)))
 dev.off()
 
-# We use one-way ANOVA to check relationship
-# between shares and days of the week
-# We first create a column 'weekday' - an ordinal categorical variable
+## We use one-way ANOVA to check what is the 
+# best day to post articles?
+# We first create a column 'weekday' 
+# - an ordinal categorical variable
 
 newsPopularity$weekday <- copy(newsPopularity$is_weekend)
 for(i in 1:nrow(newsPopularity)){
@@ -141,3 +142,121 @@ for(i in 1:nrow(newsPopularity)){
   }
 }
 newsPopularity <- newsPopularity %>% select(-wDay)
+newsPopularity_copy <- copy(newsPopularity)
+newsPopularity_copy$weekday <- as.factor(newsPopularity_copy$weekday)
+
+one_way_weekday <- aov(shares ~ weekday, data = newsPopularity_copy)
+# Levene's test to check the homogeneity of variance assumption
+
+leveneTest(shares ~ weekday, data = newsPopularity_copy)
+# Levene's test give p-value = 0.1859, not less than the
+# significant level of 0.05 -> no evidence to reject 
+# the null hypothesis - variance among groups is equal
+
+# Shapiro-Wilk test to check normality assumption
+# Since the maximum sample size is 5000, we randomly take
+# 5000 samples from the residuals
+residual <- sample(residuals(one_way_weekday), 5000)
+shapiro.test(residual)
+# p-value obtains from shapiro-wilk test is significantly < 0.01,
+# we can reject the H_0 and conclude that the data is non-normal
+# at very high certainty
+
+summary(one_way_weekday)
+png("oneway_weekday_boxplot.png", width = 1000, height = 700)
+boxplot(newsPopularity_copy$shares ~ newsPopularity_copy$weekday,
+        main = "Shares by day", xlab = "Day", ylab = "Shares",
+        col = 1:7, las = 1)
+dev.off()
+# boxplot contain outliers thus cannot portrait the box clearly
+# We have to remove outliers
+Q1 <- quantile(newsPopularity_copy$shares, .25)
+Q3 <- quantile(newsPopularity_copy$shares, .75)
+IQR <- IQR(newsPopularity_copy$shares)
+
+no_outliers <- subset(newsPopularity_copy, 
+                      newsPopularity_copy$shares > (Q1 - 1.5*IQR) &
+                        newsPopularity_copy$shares < (Q3 + 1.5*IQR))
+png("oneway_weekday_noout_boxplot.png", width = 1000, height = 700)
+boxplot(no_outliers$shares ~ no_outliers$weekday,
+        main = "Shares by day", xlab = "Day", ylab = "Shares",
+        col = 2:8, las = 1)
+dev.off()
+# From the boxplot, we can see that saturday has most shares
+# (the mean of shares is larger than the other days)
+# To confirm this, we use Tukey's HSD test to test the
+# differences between pairs of days 
+# TukeyHSD test to check difference between pairs of groups
+TukeyHSD(one_way_weekday)
+# Although the pairs 5-1 and 5-3 < 0.05 and pair 5-4 and 5-2 < 0.1
+# Pair 5-0 = 0.6327 shows that the effect of Monday and Saturday
+# are not statistically different from each other.
+# We then cannot confirm the initial conjecture that
+# Saturday is the best day
+
+
+## Similarly, we do the same procedure for data channel
+## We use one-way ANOVA to check what is the 
+# best channel to post articles?
+# We first create a column 'channel' 
+# - an ordinal categorical variable
+
+newsPopularity$channel <- copy(newsPopularity$is_weekend)
+for(i in 1:nrow(newsPopularity)){
+  if (newsPopularity$data_channel_is_lifestyle[i] == 1){
+    newsPopularity$channel[i] = 0
+  }
+  if (newsPopularity$data_channel_is_entertainment[i] == 1){
+    newsPopularity$channel[i] = 1
+  }
+  if (newsPopularity$data_channel_is_socmed[i] == 1){
+    newsPopularity$channel[i] = 2
+  }
+  if (newsPopularity$data_channel_is_tech[i] == 1){
+    newsPopularity$channel[i] = 3
+  }
+  if (newsPopularity$data_channel_is_world[i] == 1){
+    newsPopularity$channel[i] = 4
+  }
+}
+newsPopularity <- newsPopularity %>% select(-dChannel)
+newsPopularity_copy <- copy(newsPopularity)
+newsPopularity_copy$channel <- as.factor(newsPopularity_copy$channel)
+
+one_way_channel <- aov(shares ~ channel, data = newsPopularity_copy)
+# Levene's test to check the homogeneity of variance assumption
+
+leveneTest(shares ~ channel, data = newsPopularity_copy)
+# Levene's test give p-value ~= 0,  less than the
+# significant level of 0.05 -> can reject H_0
+# the null hypothesis - variance among groups is equal
+# -> variance among groups is not equal
+
+# Shapiro-Wilk test to check normality assumption
+# Since the maximum sample size is 5000, we randomly take
+# 5000 samples from the residuals
+residual <- sample(residuals(one_way_channel), 5000)
+shapiro.test(residual)
+# p-value obtains from shapiro-wilk test ~= 0,
+# we can reject the H_0 and conclude that the data is 
+# non-normal at very high certainty
+
+summary(one_way_channel)
+
+no_outliers <- subset(newsPopularity_copy, 
+                      newsPopularity_copy$shares > (Q1 - 1.5*IQR) &
+                        newsPopularity_copy$shares < (Q3 + 1.5*IQR))
+png("oneway_channel_noout_boxplot.png", width = 1000, height = 700)
+boxplot(no_outliers$shares ~ no_outliers$channel,
+        main = "Shares by channel", xlab = "Channel", ylab = "Shares",
+        col = 2:6, las = 1)
+dev.off()
+# From the boxplot, we can see that Business has most shares
+# and World has least shares
+# To confirm this, we use Tukey's HSD test to test the
+# differences between pairs of days 
+# TukeyHSD test to check difference between pairs of groups
+TukeyHSD(one_way_channel)
+# We cannot support that Business has most shares but
+# We can support World has least shares with ~=0 significance level
+
